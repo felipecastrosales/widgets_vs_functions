4cf269d88d1ff0ea27d0122659c034f3c925d895
Felipe Sales <soufeliposales@gmail.com>
2023-01-10 19:42:52 -0300

    chore: viewing logs when screen is rebuilded (comparison)

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,14 +19,22 @@ class HomePage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            // Isn't rebuilded because it's a const widget | const.
             const CustomText(
               title: 'Felipe Sales - LF7 | Widget',
             ),
+            // Is rebuilded because it's a method | not const.
             customText(
               title: 'Felipe Sales - LF7 | Method',
             ),
           ],
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {});
+        },
+        child: const Icon(Icons.refresh),
       ),
     );
   }
